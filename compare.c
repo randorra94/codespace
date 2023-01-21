093b81637e405ab81174a9cace91a851c705b355
randorra94 <104595809+randorra94@users.noreply.github.com>
2023-01-21 23:15:07 +0000

    Sat, Jan 21, 2023, 11:15 PM +00:00

--- compare.c
+++ compare.c
@@ -3,5 +3,14 @@
 
 int main(void)
 {
-    
+    int x = get_int("waht's x?\n");
+    int y = get_int("what's y?\n");
+    {
+        if (x < y)
+        printf("x less than y\n");
+        {
+            else if (x > y)
+            printf("x is greater than y\n");
+        }
+    }
 }
