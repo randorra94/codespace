7076f3dae4838ac28495971a031d7ead07c0ba16
randorra94 <104595809+randorra94@users.noreply.github.com>
2023-01-22 20:21:07 +0000

    Sun, Jan 22, 2023, 8:21 PM +00:00

--- mario.c
+++ mario.c
@@ -6,6 +6,15 @@ int main(void)
     int n;
      do
      {
-        n = get_int("size: ")
+        n = get_int("size: ");
+     }
+     while (n < 1);
+     for  (int i = 0; i < n; i++)
+     {
+        for (int j = 0; j < n; j++ )
+        {
+            printf("#");
+        }
+        printf("\n");
      }
 }
